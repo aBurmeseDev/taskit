56a705c4fa39e5c036664c742b2ec5172e085cb0
John Lwin <johnlwin.dev@gmail.com>
2021-03-28 04:36:11 -0700

    Added text box and button for new tasks

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
 import Task from './components/Task'
 
 export default function App() {
@@ -13,8 +21,28 @@ export default function App() {
 
       <View style={styles.items}>
         {/* This is where the tasks will go */}
-        <Task text={'Task'} />
+        <Task text={'Task 1'} />
+        <Task text={'Task 2'} />
+        <Task text={'Task 3'} />
+        <Task text={'Task 4'} />
       </View>
+
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+        ></TextInput>
+
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
       <StatusBar style='auto' />
     </View>
@@ -36,5 +64,32 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  items: { margin: 10 },
+  items: { marginTop: 30, marginHorizontal: 10 },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 })
